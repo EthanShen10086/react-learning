9ccb8b1aae80ef524177e1d6abae8da17918f8e7
EthanShen <ethanshen4work@gmail.com>
2024-09-16 21:00:18 +0800

    feat: 'add html select 添加select的使用'

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import { MyButton } from "./components/my-button";
 import { MyButton2 } from "./components/my-button2";
 import { ReduceCounter } from "./components/reduce-counter";
 
+const userList = [
+  {
+    name: "a",
+    value: "a",
+  },
+  {
+    name: "b",
+    value: "b",
+  },
+];
+
 function App() {
   const [count, setCount] = useState(0);
   const [isButton2, setButtonShow] = useState(false);
-  const userList = [
-    {
-      name: "a",
-      value: "a",
-    },
-    {
-      name: "b",
-      value: "b",
-    },
-  ];
+
   const userDOMList = userList.map((item, index) => {
     // 需要设置key
     return <li key={index}>{item.name}</li>;
@@ -57,36 +57,27 @@ function App() {
   };
 
   const handleChangeCount = () => {
-    setCount(count + 1);
+    // 使用函数式更新来避免闭包陷阱：
+    setCount((previousCount) => previousCount + 1);
   };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <ReduceCounter />
       <div className="m-App__content" onClick={handleChangeButton}>
         {isButton2 ? <MyButton2 /> : <MyButton />}
       </div>
+      <select placeholder="select选择框">
+        {userList.map((item, index) => 
+        // 这里直接return了 
+          <option value={item.value} key={index}>
+            {item.name}
+          </option>
+        )}
+      </select>
       <button onClick={handleFormateUserName}>{userName}</button>
       <ul> {userDOMList} </ul>
       <MyButton count={count} handleChangeCount={handleChangeCount} />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleChangeCount}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
