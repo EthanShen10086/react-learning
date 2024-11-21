712d8ce648b9ac48b0f5b6c39afadb7db68659a1
EthanShen <ethanshen4work@gmail.com>
2024-11-21 18:45:06 +0800

    feat: add GameContent component

--- src/components/tic-tac-toe.jsx
+++ src/components/tic-tac-toe.jsx
@@ -1,7 +1,10 @@
 import './tic-tac-toe/tic-tac-toe.css';
 import './tic-tac-toe/reset.css';
-import Square from './tic-tac-toe/square';
+import GameHistory from './tic-tac-toe/history';
+import GameContent from './tic-tac-toe/game-content';
+
 import { useState } from 'react';
+
 export function TicTacToe() {
 	// const baseNumberArr = [1, 2, 3];
 	// const firstRow = baseNumberArr.map((item, index) => {
@@ -18,98 +21,33 @@ export function TicTacToe() {
 	// 	{ length: 3 },
 	// 	(_, rowIndex) => componentList[rowIndex]
 	// );
-
-	// 将状态放在父组件进行控制
-	const [square, setSquare] = useState(Array(9).fill(null));
 	const [isOSign, setOSign] = useState(false);
-	const winner = getFinalWinner(square);
-	let status;
-	winner
-		? (status = 'Winner: ' + winner)
-		: (status = 'Next player: ' + (isOSign ? 'O' : 'X'));
+	// 一个二维数组
+	// [
+	// 	// Before first move
+	// 	[null, null, null, null, null, null, null, null, null],
+	// 	// After first move
+	// 	[null, null, null, null, 'X', null, null, null, null],
+	// 	// After second move
+	// 	[null, null, null, null, 'X', null, null, null, 'O'],
+	// 	// ...
+	//   ]
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const currentSquares = history[history.length - 1];
+	console.log(history, '== history', currentSquares, '== currentSquares');
 
-	const square2DComponentList = square.map((item, index) => {
-		return (
-			<Square
-				squareValue={item}
-				onSquareClick={handleClickSquare}
-				key={index}
-				squareKey={index}
-			></Square>
-		);
-	});
-	const square2DArray = Array.from({ length: 3 }, (_, rowIndex) =>
-		square2DComponentList.slice(rowIndex * 3, rowIndex * 3 + 3)
-	);
-	const ticTacToeContent = Array.from({ length: 3 }, (_, rowIndex) => {
-		// Array.from 的私有属性_是undefined
-		return (
-			// 这里如果key用item 你的item都是null啊会报prop错误
-			<section className="board-row" key={rowIndex}>
-				{square2DArray[rowIndex]}
-			</section>
-		);
-	});
-
-	// 单击 Square 时， Square 子组件现在要求 Board 父组件更新棋盘的 state。
-	// 当 Board 的 state 改变时，Board 组件和每个子 Square 都会自动重新渲染。
-	function handleClickSquare(squareIndex) {
-		// 如果已经赋值了 那么就不允许操作
-		// 如果已经有人胜利了那么也不需要操作
-		// 请注意计算整个数值数组
-		if (square[squareIndex] || getFinalWinner(square)) {
-			return;
-		}
-		// 不传递参数就是单纯不切割只纯拷贝
-		const nextSquares = square.slice();
-		// 这里的square其实不是二维数组而是数据--一维数组
-		// 二维数组的是组件是DOM元素
-		// nextSquares[0] = 'X';
-		isOSign
-			? (nextSquares[squareIndex] = 'O')
-			: (nextSquares[squareIndex] = 'X');
+	function handlePlay(nextSquares) {
+		setHistory([...history, nextSquares]);
 		setOSign(!isOSign);
-		setSquare(nextSquares);
 	}
-
-	function getFinalWinner() {
-		// 排列组合得到所有的获胜结果
-		const winResultList = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		];
-		// 便利所有胜利结果得到胜者
-		for (let rowIndex = 0; rowIndex < winResultList.length; rowIndex++) {
-			// 解构获取胜利二维数组的各个胜利结果的index
-			// 取index然后获取数据数组中对应index中的值
-			// 用来对比数值数组中的结果
-			const [
-				firstColumnInSquareIndex,
-				secondColumnInSquareIndex,
-				thirdColumnInSquareIndex,
-			] = winResultList[rowIndex];
-			if (
-				square[firstColumnInSquareIndex] &&
-				square[firstColumnInSquareIndex] ===
-					square[secondColumnInSquareIndex] &&
-				square[firstColumnInSquareIndex] === square[thirdColumnInSquareIndex]
-			) {
-				return square[firstColumnInSquareIndex];
-			}
-		}
-		return null;
-	}
-
 	return (
 		<>
-			<section className="status">{status}</section>
-			{ticTacToeContent}
+			<GameContent
+				isOSign={isOSign}
+				square={currentSquares}
+				onPlay={handlePlay}
+			/>
+			<GameHistory />
 		</>
 	);
 }
---
+++ src/components/tic-tac-toe/history.jsx
@@ -0,0 +1,9 @@
+export default function GameHistory() {
+	return (
+		<>
+			<section className="game-info">
+				<ol></ol>
+			</section>
+		</>
+	);
+}
