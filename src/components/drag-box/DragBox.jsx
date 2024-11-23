0f348203f412caf05bd8312f56970ca7b6246823
EthanShen <ethanshen4work@gmail.com>
2024-11-23 20:56:31 +0800

    fix: fix color change position change error(remember that useImmer should use (XX) => { aa = 'bb' }; no return )

--- src/components/drag-box/DragBox.jsx
+++ src/components/drag-box/DragBox.jsx
@@ -4,45 +4,44 @@ import Box from './Box.jsx';
 import { useImmer } from 'use-immer';
 
 const initialPosition = {
-  x: 0,
-  y: 0
+	x: 0,
+	y: 0,
 };
 
 export default function Canvas() {
-  const [shape, setShape] = useImmer({
-    color: 'orange',
-    position: initialPosition
-  });
+	const [shape, setShape] = useImmer({
+		color: 'orange',
+		position: initialPosition,
+	});
 
-  function handleMove(dx, dy) {
-    shape.position.x += dx;
-    shape.position.y += dy;
-  }
+	function handleMove(dx, dy) {
+		setShape((shape) => {
+			shape.position = {
+				x: (shape.position.x += dx),
+				y: (shape.position.y += dy),
+			};
+		});
+		// shape.position.x += dx;
+		// shape.position.y += dy;
+	}
 
-  function handleColorChange(e) {
-    setShape((shape) => {  shape.color = e.target.value  });
-  }
+	function handleColorChange(e) {
+		setShape((shape) => {
+			shape.color = e.target.value;
+		});
+	}
 
-  return (
-    <>
-      <select
-        value={shape.color}
-        onChange={handleColorChange}
-      >
-        <option value="orange">orange</option>
-        <option value="lightpink">lightpink</option>
-        <option value="aliceblue">aliceblue</option>
-      </select>
-      <Background
-        position={initialPosition}
-      />
-      <Box
-        color={shape.color}
-        position={shape.position}
-        onMove={handleMove}
-      >
-        Drag me!
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<select value={shape.color} onChange={handleColorChange}>
+				<option value="orange">orange</option>
+				<option value="lightpink">lightpink</option>
+				<option value="aliceblue">aliceblue</option>
+			</select>
+			<Background position={initialPosition} />
+			<Box color={shape.color} position={shape.position} onMove={handleMove}>
+				Drag me!
+			</Box>
+		</>
+	);
 }
